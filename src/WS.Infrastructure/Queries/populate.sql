9130994b04ce1d3aaebc812faf98704cd8e9c940
FrederikBA <frederikbandersen98@gmail.com>
2024-02-07 14:35:44 +0100

    remove migrations, add queries

---
+++ src/WS.Infrastructure/Queries/populate.sql
@@ -0,0 +1,13 @@
+USE KemiDB;
+INSERT INTO [dbo].[ProductGroups] ([GroupName])
+VALUES
+    ('Maling eller lak'),
+    ('Lim'),
+    ('Toiletrens'),
+    ('Kalkfjerner'),
+    ('Grundrengøring'),
+    ('Oliefjerner'),
+    ('Benzin'),
+    ('Dieselolie'),
+    ('Pletfjerner'),
+    ('Kølervæske');
