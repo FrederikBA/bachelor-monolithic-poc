9c8ebb900a4b58765ac271e19cc6bb447b0e9fb1
FrederikBA <frederikbandersen98@gmail.com>
2024-02-14 10:40:07 +0100

    frontend can build to wwwroot

--- src/WS.Web/Program.cs
+++ src/WS.Web/Program.cs
@@ -1,6 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//CORS
+const string policyName = "AllowOrigin";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policyName,
+        corsPolicyBuilder =>
+        {
+            corsPolicyBuilder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+//API Controllers
+builder.Services.AddControllers();
+
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles(); //For wwwroot frontend build
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors(policyName);
+app.UseHttpsRedirection();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
