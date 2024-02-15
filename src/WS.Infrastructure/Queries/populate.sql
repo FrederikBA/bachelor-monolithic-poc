1c903f712c4176591a1c270c0ec7e086d7c50d39
FrederikBA <frederikbandersen98@gmail.com>
2024-02-15 10:00:31 +0100

    update populate query (add producers, product status)

--- src/WS.Infrastructure/Queries/populate.sql
+++ src/WS.Infrastructure/Queries/populate.sql
@@ -10,48 +10,82 @@ VALUES
     ('Vandbehandlingskemikalier', 'Produkter, der tilsættes vand f.eks. kedelvand med henblik på at undgå korrosion, justere pH m.v.'),
     ('Andre', 'Her placeres produkter, der ikke falder ind under nogen af de ovenstående kategorier. Det vil typisk være flusmidler, analysekemikalier, trykflasker o.l.')
 
+--Product Status
+INSERT INTO [dbo].[ProductStatus] ([StatusName], [Text], [SortOrder])
+VALUES
+('OK', 'Godkendt', 5),
+('Substitution', 'Godkendt. Bør erstattes med mindre farligt produkt, da det indeholder uønskede stoffer', 6),
+('ExcludeMissingSDS', 'Er udgået pga. manglede sikkerhedsdatablad', 7),
+('ExcludeIncompleteData', 'Er udgået pga. manglende data', 8),
+('ExcludedCancelledByShippingCompanies', 'Er udgået, da det er afemdlt fra rederi/er', 9),
+('ExcludedCancelledBySupplier', 'Er udgået, da producent/leverandør ikke forhandler produktet mere', 10),
+('ExcludedReplaced', 'Er udgået fra producent/leverandør og erstattet med produkt nummer XX', 11),
+('ExcludedForbidden', 'Er forbudt at anvende (bortskaf forsvarligt)', 12),
+('ExcludedUnknownReason', 'Udgået', 13),
+('SubstitutionSeen', 'SubstitutionSeen', 14),
+('ChangeRequestReceived', 'ChangeRequestReceived', 1),
+('ChangeRequestProcessing', 'ChangeRequestProcessing', 2),
+('ChangeRequestPending', 'ChangeRequestPending', 3),
+('ChangeRequestRejected', 'ChangeRequestRejected', 4)
+
+-- Producers (and their addresses)
+-- Insert data into ProducerAddress table
+INSERT INTO ProducerAddress (Address, City, PostalCode, Country)
+VALUES 
+('Strandvejen 52', 'Copenhagen', '2100', 'Denmark'),
+('Vesterbrogade 26', 'Aarhus', '8000', 'Denmark'),
+('Oehlenschlægersgade 33', 'Odense', '5000', 'Denmark');
+
+-- Insert data into Producers table
+INSERT INTO Producers (CompanyName, ContactPerson, PhoneNumber, AddressId)
+VALUES 
+('DanishChem A/S', 'Anders Nielsen', '45-12345678', 1),
+('Nordic Chemicals ApS', 'Lise Hansen', '45-87654321', 2),
+('Scandinavian Solutions Ltd.', 'Peter Jensen', '45-33333333', 3);
+
+
 --Product Categories
 -- Inserting categories for 'Olieprodukter' group
 INSERT INTO [dbo].[ProductCategories] ([ProductGroupId], [Category])
 VALUES
-    (1, 'Motorolie'),
-    (1, 'Smøremidler'),
-    (1, 'Hydraulikolie');
+(1, 'Motorolie'),
+(1, 'Smøremidler'),
+(1, 'Hydraulikolie');
 
 -- Inserting categories for 'Rengørngsmidler' group
 INSERT INTO [dbo].[ProductCategories] ([ProductGroupId], [Category])
 VALUES
-    (2, 'Almindelige rengøringsmidler'),
-    (2, 'Desinfektionsmidler'),
-    (2, 'Industrielle rengøringsmidler');
+(2, 'Almindelige rengøringsmidler'),
+(2, 'Desinfektionsmidler'),
+(2, 'Industrielle rengøringsmidler');
 
 -- Inserting categories for 'Maling, lak, lim eller fugemasse' group
 INSERT INTO [dbo].[ProductCategories] ([ProductGroupId], [Category])
 VALUES
-    (3, 'Industriel maling'),
-    (3, 'Lak'),
-    (3, 'Lim');
+(3, 'Industriel maling'),
+(3, 'Lak'),
+(3, 'Lim');
 
 -- Inserting categories for 'Brændstof og tilsætningsstoffer til brændstof' group
 INSERT INTO [dbo].[ProductCategories] ([ProductGroupId], [Category])
 VALUES
-    (4, 'Benzin'),
-    (4, 'Diesel'),
-    (4, 'Tilsætningsstoffer til brændstof');
+(4, 'Benzin'),
+(4, 'Diesel'),
+(4, 'Tilsætningsstoffer til brændstof');
 
 -- Inserting categories for 'Vandbehandlingskemikalier' group
 INSERT INTO [dbo].[ProductCategories] ([ProductGroupId], [Category])
 VALUES
-    (5, 'Filtreringsmidler'),
-    (5, 'Desinfektionsmidler til vand'),
-    (5, 'pH-reguleringsmidler');
+(5, 'Filtreringsmidler'),
+(5, 'Desinfektionsmidler til vand'),
+(5, 'pH-reguleringsmidler');
 
 -- Inserting categories for 'Andre' group
 INSERT INTO [dbo].[ProductCategories] ([ProductGroupId], [Category])
 VALUES
-    (6, 'Specialkemikalier'),
-    (6, 'Industrikemikalier'),
-    (6, 'Husholdningskemikalier');
+(6, 'Specialkemikalier'),
+(6, 'Industrikemikalier'),
+(6, 'Husholdningskemikalier');
 
 -- Warning Types
 INSERT INTO [dbo].[WarningTypes] ([Type], [Priority])
