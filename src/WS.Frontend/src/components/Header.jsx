1f4f775ead5fff8a4a13e33f0a09fe28fce2c1e1
FrederikBA <frederikbandersen98@gmail.com>
2024-02-15 14:30:24 +0100

    restyle header/navbar

--- src/WS.Frontend/src/components/Header.jsx
+++ src/WS.Frontend/src/components/Header.jsx
@@ -1,17 +1,32 @@
-import { NavLink } from "react-router-dom";
-
+import React, { useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const Header = () => {
+    const [activeItem, setActiveItem] = useState("SEA HEALTH & WELFARE APPLICATIONS");
+
+    const handleSelect = (eventKey) => {
+        setActiveItem(eventKey);
+    };
+
     return (
-        <div>
-            <nav>
-                <ul className='header'>
-                    <li><NavLink to='/'>One</NavLink></li>
-                    <li><NavLink to='/two'>Two</NavLink></li>
-                </ul>
-            </nav>
-        </div >
-    )
-}
+        <div className="header">
+            <Dropdown onSelect={handleSelect}>
+                <Dropdown.Toggle className="nav-dropdown" variant="success" id="dropdown-basic">
+                    <img src="/icons/seahealthicon.svg" alt="Sea Health Icon" className="icon" />
+                    SEA HEALTH & WELFARE <span className="bold">APPLICATIONS</span>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu className="nav-dropdown-menu">
+                    <Dropdown.Item eventKey="SEA HEALTH & WELFARE APPLICATIONS">
+                        <img src="/icons/seahealthicon.svg" alt="Sea Health Icon" className="icon" />
+                        SEA HEALTH & WELFARE <span className="bold">APPLICATIONS</span>
+                    </Dropdown.Item>
+                    <Dropdown.Item eventKey="Chemicals">Chemicals</Dropdown.Item>
+                    <Dropdown.Item eventKey="Warning Sentences">Warning Sentences</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    );
+};
 
 export default Header;
