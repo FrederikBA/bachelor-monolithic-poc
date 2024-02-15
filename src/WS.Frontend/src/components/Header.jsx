fd5fbe359510b3c4dad46eab198ef290a2e2819c
Frederik Andersen <54848880+FrederikBA@users.noreply.github.com>
2024-02-15 15:18:13 +0100

    Feature/updatefrontenddesign (#3)
    * restyle header/navbar
    
    * header styling
    
    * add basic routing from new nav

--- src/WS.Frontend/src/components/Header.jsx
+++ src/WS.Frontend/src/components/Header.jsx
@@ -1,17 +1,43 @@
-import { NavLink } from "react-router-dom";
-
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const Header = () => {
+    const [activeItem, setActiveItem] = useState("SEA HEALTH & WELFARE APPLICATIONS");
+
+    const handleSelect = (eventKey) => {
+        setActiveItem(eventKey);
+    };
+
     return (
-        <div>
-            <nav>
-                <ul className='header'>
-                    <li><NavLink to='/'>One</NavLink></li>
-                    <li><NavLink to='/two'>Two</NavLink></li>
-                </ul>
-            </nav>
-        </div >
-    )
-}
+        <div className="header">
+            <Dropdown onSelect={handleSelect}>
+                <Dropdown.Toggle className="nav-dropdown" variant="success" id="dropdown-basic">
+                    <img src="/icons/seahealthicon.svg" alt="Sea Health Icon" className="icon" />
+                    SEA HEALTH & WELFARE <span className="bold">APPLICATIONS</span>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu className="nav-dropdown-menu">
+                    <NavLink to="/" className="nav-link-custom">
+                        <Dropdown.Item as="li" className="nav-dropdown-item" eventKey="SEA HEALTH & WELFARE APPLICATIONS">
+                            <img src="/icons/seahealthicon.svg" alt="Sea Health Icon" className="icon dropdown-item-icon" />
+                            SEA HEALTH & WELFARE <span className="bold">APPLICATIONS</span>
+                        </Dropdown.Item>
+                    </NavLink>
+                    <NavLink to="/chemicals" className="nav-link-custom">
+                        <Dropdown.Item as="li" className="nav-dropdown-item nav-dropdown-item-bold" eventKey="CHEMICALS">
+                            <NavLink to="/chemicals" className="nav-link-custom">CHEMICALS</NavLink>
+                        </Dropdown.Item>
+                    </NavLink>
+                    <NavLink to="/chemicals" className="nav-link-custom">
+                        <Dropdown.Item as="li" className="nav-dropdown-item nav-dropdown-item-bold" eventKey="WARNING SENTENCES">
+                            <NavLink to="/warningsentences" className="nav-link-custom">WARNING SENTENCES</NavLink>
+                        </Dropdown.Item>
+                    </NavLink>
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    );
+};
 
 export default Header;
