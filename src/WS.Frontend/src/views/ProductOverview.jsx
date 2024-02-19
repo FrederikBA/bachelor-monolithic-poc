18d7b7b8ef6cba792d9d013dc2606970a6fe4445
FrederikBA <frederikbandersen98@gmail.com>
2024-02-19 11:23:58 +0100

    get products from productService

--- src/WS.Frontend/src/views/ProductOverview.jsx
+++ src/WS.Frontend/src/views/ProductOverview.jsx
@@ -1,7 +1,26 @@
 import ActionBar from "../components/actions/ActionBar"
 import ShwSpinner from "../components/spinners/ShwSpinner"
+import productService from "../services/productService"
+import apiUtils from "../utils/apiUtils"
+
+
+import { useState, useEffect } from 'react'
 
 const ProductOverview = () => {
+    const [products, setProducts] = useState({});
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const response = await productService.getAllproducts()
+                setProducts(response);
+                console.log(response);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getProducts()
+    }, []);
 
     return (
         <div className="center">
