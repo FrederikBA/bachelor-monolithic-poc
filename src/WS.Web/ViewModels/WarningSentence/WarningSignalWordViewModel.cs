2a90741b8112716aded6834d1448dade32d91fdf
FrederikBA <frederikbandersen98@gmail.com>
2024-02-20 11:19:36 +0100

    last viewmodel updates

--- src/WS.Web/ViewModels/WarningSentence/WarningSignalWordViewModel.cs
+++ src/WS.Web/ViewModels/WarningSentence/WarningSignalWordViewModel.cs
@@ -3,5 +3,4 @@ namespace WS.Web.ViewModels.WarningSentence;
 public class WarningSignalWordViewModel
 {
     public string? SignalWordText { get; set; }
-    public int Priority { get; set; }    
 }
