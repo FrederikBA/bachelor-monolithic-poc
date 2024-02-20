867b3ee3de4e94b8191a647ecb78ebf6a14ec49a
FrederikBA <frederikbandersen98@gmail.com>
2024-02-20 16:38:01 +0100

    add viewmodel service function

--- src/WS.Web/Services/WarningSentenceViewModelService.cs
+++ src/WS.Web/Services/WarningSentenceViewModelService.cs
@@ -53,4 +53,45 @@ public class WarningSentenceViewModelService : IWarningSentenceViewModelService
 
         return warningSentenceViewModels;
     }
+
+    public async Task<WarningSentenceViewModel> GetWarningSentenceViewModel(int id)
+    {
+        var warningSentenceEntity = await _warningSentenceService.GetWarningSentenceByIdAsync(id);
+        
+        var warningSentenceViewModel = new WarningSentenceViewModel
+        {
+            Id = warningSentenceEntity.Id,
+            Code = warningSentenceEntity.Code,
+            Text = warningSentenceEntity.Text,
+            
+            //Warning Category
+            WarningCategory = new WarningCategoryViewModel
+            {
+                Text = warningSentenceEntity.WarningCategory!.Text,
+                
+                //Warning Type
+                WarningType = new WarningTypeViewModel
+                {
+                    Type = warningSentenceEntity.WarningCategory!.WarningType!.Type,
+                }
+            },
+            
+            //Warning Pictogram
+            WarningPictogram = new WarningPictogramViewModel
+            {
+                Code = warningSentenceEntity.WarningPictogram!.Code,
+                Text = warningSentenceEntity.WarningPictogram!.Text,
+                Pictogram = warningSentenceEntity.WarningPictogram!.Pictogram,
+                Extension = warningSentenceEntity.WarningPictogram!.Extension,
+            },
+            
+            //Warning Signal Word
+            WarningSignalWord = new WarningSignalWordViewModel
+            {
+                SignalWordText = warningSentenceEntity.WarningSignalWord!.SignalWordText,
+            }
+        };
+        
+        return warningSentenceViewModel;
+    }
 }
