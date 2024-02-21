f932fbad822e61afc275c694c068802beb3df23c
FrederikBA <frederikbandersen98@gmail.com>
2024-02-21 12:42:24 +0100

    pictograms as svg, initiate WS view

--- src/WS.Frontend/src/views/WarningSentenceView.jsx
+++ src/WS.Frontend/src/views/WarningSentenceView.jsx
@@ -1,5 +1,10 @@
 import ActionBarWarningSentencesView from '../components/actions/ActionBarWarningSentencesView';
 import warningSentenceService from '../services/warningSentenceService';
+import ShwSpinner from "../components/spinners/ShwSpinner";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Table } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -10,7 +15,6 @@ const WarningSentenceView = () => {
 
     const navigate = useNavigate();
     const { sentenceId } = useParams();
-
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,12 +27,12 @@ const WarningSentenceView = () => {
             } finally {
                 setTimeout(() => {
                     setLoading(false);
-                }, 1000);
+                }, 100);
             }
         };
 
         fetchData();
-    }, []);
+    }, [sentenceId]);
 
     const editWarningSentence = () => {
         console.log("Edit button pressed");
@@ -39,14 +43,53 @@ const WarningSentenceView = () => {
     }
 
     return (
-        <div className="center">
+        <div>
             <ActionBarWarningSentencesView
                 editAction={editWarningSentence}
                 backAction={navigateBack}
             />
-            <h1>Warning Sentence View: {sentenceId}</h1>
+            {loading ? null : (
+                <div className="ws-view-top-section">
+                    <Container className="ws-view-container">
+                        <Row>
+                            <Col xs={2}>
+                            </Col>
+                            <Col className="headline" xs={2}>
+                                <span className="area-title">{warningSentence.code}</span>
+                            </Col>
+                            <Col xs={2} className="pictogram-col">
+                                <img src={`/pictograms/${warningSentence.warningPictogram.pictogram}.${warningSentence.warningPictogram.extension}`} alt="Piktogram" className="view-pictogram" />
+                            </Col>
+
+                            <Col xs={6}>
+
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={2}></Col>
+                            <Col xs={8}>
+                                <div className="area-title-content">
+                                    <span>{warningSentence.text}</span>
+                                </div>
+                            </Col>
+                            <Col xs={2}></Col>
+                        </Row>
+                    </Container>
+                </div>
+            )}
+            {loading && (
+                <div className="ws-view-top-section">
+                    <Container className="ws-view-container">
+                        <Row className="justify-content-center align-items-center">
+                            <Col xs={12} className="text-center">
+                                <ShwSpinner />
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            )}
         </div>
     )
 }
 
-export default WarningSentenceView
+export default WarningSentenceView;
