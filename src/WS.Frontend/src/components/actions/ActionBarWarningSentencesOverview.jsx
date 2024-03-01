3a896dcabd3ad6258910958e2c7b3aa9b5bbd39b
FrederikBA <frederikbandersen98@gmail.com>
2024-03-01 10:08:04 +0100

    Frontend - Add rename icon on warning sentence action bar

--- src/WS.Frontend/src/components/actions/ActionBarWarningSentencesOverview.jsx
+++ src/WS.Frontend/src/components/actions/ActionBarWarningSentencesOverview.jsx
@@ -2,6 +2,7 @@ import { ReactComponent as CloneIcon } from "../../assets/cloneicon.svg";
 import { ReactComponent as SelectAllIcon } from "../../assets/selectallicon.svg";
 import { ReactComponent as DeleteIcon } from "../../assets/deleteicon.svg";
 import { ReactComponent as NewIcon } from "../../assets/newicon.svg";
+import { ReactComponent as EditIcon } from "../../assets/editicon.svg";
 import ActionButton from "./ActionButton";
 import ActionBarDivision from "./ActionBarDivision"
 
@@ -10,6 +11,7 @@ const ActionBarWarningSentencesOverview = ({ selectAllAction, action, hasChecked
         <div className="action-bar" style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
             <ActionButton icon={<NewIcon />} text="Ny H-sætning" action={action} />
             <ActionButton icon={<CloneIcon />} text="Kopier" action={action} disabled={!hasCheckedSentences} />
+            <ActionButton icon={<EditIcon />} text="Omdøb" action={action} disabled={!hasCheckedSentences} />
             <ActionButton icon={<DeleteIcon />} text="Slet" action={action} disabled={!hasCheckedSentences} />
             <ActionBarDivision />
             <ActionButton icon={<SelectAllIcon />} text="Vælg alle" action={selectAllAction} />
