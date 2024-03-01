f341778abbd6debd194c4ead1ee6ca2aacaa5017
FrederikBA <frederikbandersen98@gmail.com>
2024-03-01 09:41:22 +0100

    add endpoint

--- src/WS.Web/Controllers/WarningSentenceController.cs
+++ src/WS.Web/Controllers/WarningSentenceController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using WS.Core.Interfaces.DomainServices;
+using WS.Core.Models.Dtos;
 using WS.Web.Interfaces;
 
 namespace WS.Web.Controllers;
@@ -8,10 +10,12 @@ namespace WS.Web.Controllers;
 public class WarningSentenceController : ControllerBase
 {
     private readonly IWarningSentenceViewModelService _warningSentenceViewModelService;
+    private readonly IWarningSentenceService _warningSentenceService;
 
-    public WarningSentenceController(IWarningSentenceViewModelService warningSentenceViewModelService)
+    public WarningSentenceController(IWarningSentenceViewModelService warningSentenceViewModelService, IWarningSentenceService warningSentenceService)
     {
         _warningSentenceViewModelService = warningSentenceViewModelService;
+        _warningSentenceService = warningSentenceService;
     }
     
     [HttpGet("all")]
@@ -25,6 +29,13 @@ public class WarningSentenceController : ControllerBase
     public async Task<IActionResult> GetWarningSentence(int id)
     {
         var warningSentence = await _warningSentenceViewModelService.GetWarningSentenceViewModel(id);
+        return Ok(warningSentence);
+    }
+    
+    [HttpPost("add")]
+    public async Task<IActionResult> AddWarningSentence(WarningSentenceDto warningSentenceDto)
+    {
+        var warningSentence = await _warningSentenceService.AddWarningSentenceAsync(warningSentenceDto);
         return Ok(warningSentence);
     }
 }
