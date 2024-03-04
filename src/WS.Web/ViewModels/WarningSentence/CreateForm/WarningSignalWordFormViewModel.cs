d15e81102dd82dbb50a0f289ae4f65dc7ce641b1
FrederikBA <frederikbandersen98@gmail.com>
2024-03-04 19:29:21 +0100

    add form viewmodels

---
+++ src/WS.Web/ViewModels/WarningSentence/CreateForm/WarningSignalWordFormViewModel.cs
@@ -0,0 +1,7 @@
+namespace WS.Web.ViewModels.WarningSentence.CreateForm;
+
+public class WarningSignalWordFormViewModel
+{
+    public int Id { get; set; }
+    public string? SignalWordText { get; set; }
+}
