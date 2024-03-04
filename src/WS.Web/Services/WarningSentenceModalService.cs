24d0ddc533333c5dbeb8273abd4a6cd172a210bd
FrederikBA <frederikbandersen98@gmail.com>
2024-03-04 09:09:58 +0100

    add web project support for modal controller

---
+++ src/WS.Web/Services/WarningSentenceModalService.cs
@@ -0,0 +1,12 @@
+using WS.Web.Interfaces;
+using WS.Web.ViewModels.WarningSentence;
+
+namespace WS.Web.Services;
+
+public class WarningSentenceModalService : IWarningSentenceModalService
+{
+    public Task<WarningSentenceBaseViewModel> GetWarningSentenceModalAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
